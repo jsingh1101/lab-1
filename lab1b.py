4d31bcff2a4ca47f40633327a0dfcebf7c501e6b
jsingh1101_seneca <jsingh1101@myseneca.ca>
2024-09-18 02:02:38 -0400

    Added lab files and captured output

---
+++ lab1b.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Create a string object
+name = 'Isaac'
+
+# Print the value of the string object
+print(name)
+
+# Print the string literal 'name' (this prints the word 'name', not the variable)
+print('name')
+
+# Print concatenated string with variable value
+print('I have a friend named ' + name)
+
+# Print question with the string value
+print('How old are you ' + name + '?')
---
+++ lab1d.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Define integer objects
+x = 10
+y = 2
+z = 5
+
+# Print mathematical operations
+print(f'{x} + {y} * {z} = {x + y * z}')
