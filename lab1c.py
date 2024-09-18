2a5733667cf395577895df8719a2e88e2af1adbc
jsingh1101_seneca <jsingh1101@myseneca.ca>
2024-09-18 01:48:03 -0400

    Added lab1c.py for integer objects

---
+++ lab1c.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Create integer objects
+num1 = 5
+num2 = 10
+
+# Print the values of the integer objects
+print(num1)
+print(num2)
+
+# Create a new object 'sum' for the addition of num1 and num2
+sum = num1 + num2
+
+# Print the sum
+print(sum)
+
+# Print concatenated string with integer (convert integer to string first)
+print('The sum is: ' + str(sum))
+
+# Define name and age objects
+name = 'Isaac'
+age = 72
+
+# Print statement with name and age
+print(name + ' is ' + str(age) + ' years old!')
